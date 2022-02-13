4283f36980c091fa227c2a2af0b6bcd104141d80
mullatoez <kimanthikevin76@gmail.com>
2022-02-13 10:01:17 +0300

    added routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cool_app/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
@@ -26,45 +27,42 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-
   int _counter = 0;
 
-  void increaseCounter(){
+  void increaseCounter() {
     setState(() {
       _counter++;
     });
   }
 
-  void showToast(){
-      Fluttertoast.showToast(msg: 'Counter is less than 1', 
-      fontSize: 18);
-    }
+  void showToast() {
+    Fluttertoast.showToast(msg: 'Counter is less than 1', fontSize: 18);
+  }
 
-  void resetCounter(){
+  void resetCounter() {
     setState(() {
       _counter = 0;
     });
   }
 
-  void decreaseCounter(){
+  void decreaseCounter() {
     setState(() {
-      if(_counter > 0){
+      if (_counter > 0) {
         setState(() {
           _counter--;
         });
-      }
-      else{
+      } else {
         print('Counter is less than 1');
         showToast();
       }
     });
   }
 
-  List<Color> _bg_colors = [
-    Colors.greenAccent,
-    Colors.red,
-    Colors.blue
-  ];
+  // ignore: non_constant_identifier_names
+  final List<Color> _bg_colors = [Colors.greenAccent, Colors.red, Colors.blue];
+
+  TextEditingController _controller = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -73,17 +71,75 @@ class _HomeViewState extends State<HomeView> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Tapped button this many times'),
-            Text(
-              '$_counter',
-              style: TextStyle(fontSize: 18),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 300,
+            height: 300,
+            child: Image.asset('assets/flutter_final.png'),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextFormField(
+              controller: _controller,
+              cursorColor: Colors.black,
+              decoration: InputDecoration(
+                hintText: 'Input Username',
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              cursorColor: Colors.black,
+              decoration: InputDecoration(
+                hintText: 'Input Password',
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width / 1.3,
+            height: 40,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30.0),
+              color: Colors.blueGrey,
+            ),
+            child: MaterialButton(
+              onPressed: () {
+                print('Username: ' + _controller.text);
+
+                //route to a new Page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomePage(),
+                  ),
+                );
+              },
+              child: Text('Press Me'),
+            ),
+          ),
+          const Text('Tapped button this many times'),
+          Text(
+            '$_counter',
+            style: TextStyle(fontSize: 18),
+          ),
+        ],
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
